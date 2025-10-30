e1ad8b6322426742ff28b51868a5c1ebef380ccc
Matthias Mühlender <muehlenderm@googlemail.com>
2025-10-30 23:54:24 +0100

    update Abschnitt_27

--- Abschnitt_27/index.php
+++ Abschnitt_27/index.php
@@ -12,29 +12,86 @@
   <?php
     $name = "Matthias";
 // echo
-    echo "Ich bin ein echo und habe keinen Rückgabewert <br>";
+    echo "Ich bin ein <b>echo</b> und habe <u>keinen</u> Rückgabewert <br>";
 // print
-    $test = print "Ich bin ein print und kann einen Rückgabewert liefern";
+    $test = print "Ich bin ein <b>print</b> und kann einen Rückgabewert liefern";
     echo $test;
 
 // heredoc
-$text = <<<TEXT
-<br>
-Hallo ich kann hier alles schreiben "test"
-**test** und bin in einer Zeile bis
-<br>
-oh ein br-tag dazwischen gesetzt wurde von <u>$name</u>
+    $text = <<<TEXT
+      <br>
+      Hallo ich kann mit <b>heredoc</b> hier alles schreiben "test"
+      **test** und bin in einer Zeile bis
+      <br>
+      oh ein br-tag dazwischen gesetzt wurde von <u>$name</u>
+    TEXT;
 
-TEXT;
+    echo $text;
 
-echo $text
+// nowdoc
+    $text2 = <<<'TEXT2'
+      <br>
+      <b>nowdoc</b> das selbe nur das hier <u>keine</u> Variablen verwendet werden können
+      da es hier drin alles als <u>string</u> gewertet wird
+    TEXT2;
 
+    echo $text2;
+    
+// printf
+
+    printf("<br>Hallo mein Name ist %s und ich habe %d %s: Ein <u>string</u> Formatiert über Platzhalter ausgegeben", $name, 6, "Äpfel");
+
+/* 
+%s: Zeichensätze
+%d: oder %i: Ganzzahl
+%f: Fließkommazahl (Standartmäßig 6 Dezimalstellen)
+%x: Hexadezimalzahl (kleinbuchstaben)
+%X: Hexadezimalzahl (GROßBUCHSTABEN)
+%b: Binärzahl
+%%: Ein Literal-Prozentzeichen
+*/
+echo "<br>";
+// print_r
+
+    $array = ["Jonas", "Jessica", "eva", 35, true, false, [1,2,3,4,5]];
+
+    print_r($array);
+    echo"<br>bei print_r werden array's oder objekte ausgegeben und ist eher ein Debugging-tool <br>";
+
+// var_dump
+
+    $integer = 42;
+    $float = 3.15;
+    $string = "Hallo String";
+    $array2 = [1,2,3,"stringtext",4,5];
+    $object = (object) $array;
+
+    var_dump($array2);
+    // Debbuging-tool nr.1
+    echo "<br>";
+    echo "<br>";
+// var_export
+
+    var_export($array2);
+    echo "<br>";
+    print_r ($array2);
+    echo "<br>";
+    var_export($string);
+    echo "<br>";
+    var_export($integer);
+    echo "<br>";
+
+    $value = var_export($integer, true);
+    echo "<br>";
+    echo $value;
+    echo $value;
+    echo $value;
+    echo $value;
+    echo $value;
+    echo $value;
+    echo $value;
+    
   ?>
   
-
-
-  
-
-
 </body>
 </html>
