053a64779b8be12bdf84d30df8a1cbcd99590e64
Matthias Mühlender <muehlenderm@googlemail.com>
2025-11-01 23:54:18 +0100

    Update GetType

---
+++ Abschnitt_28/GetType/Index.php
@@ -0,0 +1,20 @@
+<?php
+    $a = false;
+    $b = 34;
+    $c = 34.12184;
+    $d = "Servus was los?";
+    $e = array(
+      "Protokolle",
+      12,
+      $f = array(
+          "Werte",
+          "Grundprinzipien",
+          "Vollkommenheitsgesetze"
+      ),
+      12184.34,
+      true
+    );
+
+echo gettype($a);
+
+?>
