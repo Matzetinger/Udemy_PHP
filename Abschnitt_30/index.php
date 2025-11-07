1a1430f9b75532d5dd334551199822e83bb1e74a
Matthias Mühlender <muehlenderm@googlemail.com>
2025-11-07 23:56:58 +0100

    Update

--- Abschnitt_30/index.php
+++ Abschnitt_30/index.php
@@ -14,7 +14,7 @@
     <div class="inhalt-container">
 
       <!-- Addition -->
-      <div>
+      <div class="box">
         <h3>Addition</h3>
         <?php 
           $a = 5;
@@ -22,23 +22,23 @@
           $c = 10;
           $summe = $a + $b;
         ?>
-        <p>$a = <?= $a ?> <br>$b = <?= $b ?> <br>$summe = $a + $b = <?= $summe ?></p>
+        <p>$a = 5 <br>$b = 3 <br>$summe = $a + $b = <span><?= $summe ?></span></p>
 
         <?php $c += 5; ?>
         <h5>Short</h5>
-        <p>$c = 10 <br>$c += 5 = <?= $c ?></p>
+        <p>$c = 10 <br>$c += 5 = <span><?= $c ?></span></p>
 
         <?php $c += 5; ?>
-        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <span><?= $c ?></span></p>
 
         <?php $c++; ?>
         <h5>Increment</h5>
-        <p>Hier wird das vorherige Ergebnis +1 erhöht <br>$c++ = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis +1 erhöht <br>$c++ = <span><?= $c ?></span></p>
 
       </div>
 
       <!-- Subtraktion -->
-      <div>
+      <div class="box">
         <h3>Subtraktion</h3>
         <?php 
           $a = 5;
@@ -46,23 +46,23 @@
           $c = 10;
           $summe = $a - $b;
         ?>
-        <p>$a = <?= $a ?> <br>$b = <?= $b ?> <br>$summe = $a - $b = <?= $summe ?></p>
+        <p>$a = 5 <br>$b = 3 <br>$summe = $a - $b = <span><?= $summe ?></span></p>
 
         <?php $c -= 5; ?>
         <h5>Short</h5>
-        <p>$c = 10 <br>$c -= 5 = <?= $c ?></p>
+        <p>$c = 10 <br>$c -= 5 = <span><?= $c ?></span></p>
 
         <?php $c += 5; ?>
-        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <span><?= $c ?></span></p>
 
         <?php $c--; ?>
         <h5>Decrement</h5>
-        <p>Hier wird das vorherige Ergebnis -1 genommen <br>$c-- = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis -1 genommen <br>$c-- = <span><?= $c ?></span></p>
 
       </div>
 
       <!-- Multiplikation -->
-      <div>
+      <div class="box">
         <h3>Multiplikation</h3>
         <?php 
           $a = 5;
@@ -70,23 +70,23 @@
           $c = 10;
           $summe = $a * $b;
         ?>
-        <p>$a = <?= $a ?> <br>$b = <?= $b ?> <br>$summe = $a * $b = <?= $summe ?></p>
+        <p>$a = 5 <br>$b = 3 <br>$summe = $a * $b = <span><?= $summe ?></span></p>
 
         <?php $c *= 5; ?>
         <h5>Short</h5>
-        <p>$c = 10 <br>$c *= 5 = <?= $c ?></p>
+        <p>$c = 10 <br>$c *= 5 = <span><?= $c ?></span></p>
 
         <?php $c += 5; ?>
-        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <span><?= $c ?></span></p>
 
         <?php $c--; ?>
         <h5>Decrement</h5>
-        <p>Hier wird das vorherige Ergebnis -1 genommen <br>$c-- = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis -1 genommen <br>$c-- = <span><?= $c ?></span></p>
 
       </div>
 
       <!-- Division -->
-      <div>
+      <div class="box">
         <h3>Division</h3>
         <?php 
           $a = 5;
@@ -94,23 +94,23 @@
           $c = 10;
           $summe = $a / $b;
         ?>
-        <p>$a = <?= $a ?> <br>$b = <?= $b ?> <br>$summe = $a / $b = <?= $summe ?></p>
+        <p>$a = 5 <br>$b = 3 <br>$summe = $a / $b = <span><?= $summe ?></span></p>
 
         <?php $c /= 5; ?>
         <h5>Short</h5>
-        <p>$c = 10 <br>$c /= 5 = <?= $c ?></p>
+        <p>$c = 10 <br>$c /= 5 = <span><?= $c ?></span></p>
 
         <?php $c += 5; ?>
-        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <span><?= $c ?></span></p>
 
         <?php $c--; ?>
         <h5>Decrement</h5>
-        <p>Hier wird das vorherige Ergebnis -1 genommen <br>$c-- = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis -1 genommen <br>$c-- = <span><?= $c ?></span></p>
 
       </div>
 
       <!-- Modulo -->
-      <div>
+      <div class="box">
         <h3>Modulo (rest)</h3>
         <?php 
           $a = 5;
@@ -118,18 +118,18 @@
           $c = 10;
           $summe = $a % $b;
         ?>
-        <p>$a = <?= $a ?> <br>$b = <?= $b ?> <br> $summe = $a % $b = <?= $summe ?></p>
+        <p>$a = 5 <br>$b = 3 <br> $summe = $a % $b = <span><?= $summe ?></span></p>
 
         <?php $c %= 5; ?>
         <h5>Short</h5>
-        <p>$c = 10 <br>$c %= 5 = <?= $c ?></p>
+        <p>$c = 10 <br>$c %= 5 = <span><?= $c ?></span></p>
 
         <?php $c += 5; ?>
-        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis + genommen <br>$c += 5 = <span><?= $c ?></span></p>
 
         <?php $c++; ?>
         <h5>Inkrement</h5>
-        <p>Hier wird das vorherige Ergebnis +1 erhöht <br>$c++ = <?= $c ?></p>
+        <p>Hier wird das vorherige Ergebnis +1 erhöht <br>$c++ = <span><?= $c ?></span></p>
 
       </div>
 
@@ -141,40 +141,85 @@
     <h2>2. Post und Prä Ink- und Decrement</h2>
     <div class="inhalt-container">
 
-      <div>
+      <div class="box">
         <h3>Post-Inkrement (danach)</h3>
         <?php 
           $d = 100;
           $e = $d++;
         ?>
-        <p>$d = 100 <br>$e = $d++ <br>e = <?= $e ?> <br>d = <?= $d ?></p>
+        <p>$d = 100 <br>$e = $d++ <br>e = <span><?= $e ?></span> <br>d = <span><?= $d ?></span></p>
       </div>
 
-      <div>
+      <div class="box">
         <h3>Post-Decrement (danach)</h3>
         <?php 
           $d = 100;
           $e = $d--;
         ?>
-        <p>$d = 100 <br>$e = $d-- <br>e = <?= $e ?> <br>d = <?= $d ?></p>
+        <p>$d = 100 <br>$e = $d-- <br>e = <span><?= $e ?></span> <br>d = <span><?= $d ?></span></p>
       </div>
 
-      <div>
+      <div class="box">
         <h3>Prä-Inkrement (davor)</h3>
         <?php 
           $d = 100;
           $e = ++$d;
         ?>
-        <p>$d = 100 <br>$e = ++$d <br>e = <?= $e ?> <br>d = <?= $d ?></p>
+        <p>$d = 100 <br>$e = ++$d <br>e = <span><?= $e ?></span> <br>d = <span><?= $d ?></span></p>
       </div>
 
-      <div>
+      <div class="box">
         <h3>Prä-Decrement (davor)</h3>
         <?php 
           $d = 100;
           $e = --$d;
         ?>
-        <p>$d = 100 <br>$e = --$d <br>e = <?= $e ?> <br>d = <?= $d ?></p>
+        <p>$d = 100 <br>$e = --$d <br>e = <span><?= $e ?></span> <br>d = <span><?= $d ?></span></p>
+      </div>
+
+    </div>
+  </section>
+
+  <section class="section3">
+   <h2>3. Expotential-Operator</h2>
+ <div class="inhalt-container">
+
+      <div class="box">
+        <h3>Potenzrechnung</h3>
+        <?php 
+          $basis = 2;
+          $exponent = 3;
+
+          $result = $basis ** $exponent;
+        ?>
+        <p>$basis = 2 <br> $exponent = 3 <br> $result = $basis ** $exponent = <span><?= $result ?></span></p>
+      </div>
+
+      <div class="box">
+        <h3>Quadrat ausrechnen</h3>
+        <?php 
+          $zahl = 4;
+          $quadrat = $zahl ** 2;
+        ?>
+        <p>$zahl = 4 <br> $quadrat = $zahl ** 2 = <span><?= $quadrat ?></span></p>
+      </div>
+
+      <div class="box">
+        <h3>Kubik ausrechnen</h3>
+        <?php 
+          $zahl = 3;
+          $kubik = $zahl ** 3;
+        ?>
+        <p>$zahl = 3 <br> $kubik = $zahl ** 3 = <span><?= $kubik ?></span></p>
+      </div>
+
+      <div class="box">
+        <h3>Wurzel ausrechnen</h3>
+        <?php 
+          $zahl = 16;
+          $wurzel = $zahl ** (1/2);
+        ?>
+        <p>$zahl = 16 <br> $wurzel = $zahl ** (1/2) = <span><?= $wurzel ?></span></p>
       </div>
 
     </div>
