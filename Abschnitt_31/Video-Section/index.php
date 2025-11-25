481290162caa2bad79f6a212c2d94193b7ef9c66
Matthias Mühlender <muehlenderm@googlemail.com>
2025-11-25 23:30:15 +0100

    Update

--- Abschnitt_31/Video-Section/index.php
+++ Abschnitt_31/Video-Section/index.php
@@ -21,8 +21,8 @@
         <li><a href="#3">03. Strings vergleichen</a></li>
         <li><a href="#4">04. Strings alphabetisch sortieren</a></li>
         <li><a href="#5">05. Stringlänge strlen</a></li>
-        <li><a href="#6">06. Stringposition strpos</a></li>
-        <li><a href="#7">07. </a></li>
+        <li><a href="#6">06. Stringposition strpos - stripos</a></li>
+        <li><a href="#7">07. Wort / Zahl ersetzen str_replace - str_ireplace</a></li>
         <li><a href="#8">08. </a></li>
         <li><a href="#9">09. </a></li>
         <li><a href="#10">10. </a></li>
@@ -182,12 +182,12 @@
   <!-- 6. Stringposition strpos -->
   <section id="6" class="section6">
     <div class="text-box">
-      <h2>6. Stringposition strpos</h2>
+      <h2>6. Stringposition strpos - stripos</h2>
       <h6><a href="#0">Inhaltsverzeichnis</a></h6>
     </div>
     <div class="inhalt-container">
 
-      <!-- strlen -->
+      <!-- strpos -->
       <div class="box">
         <h4>strpos</h4>
         <?php 
@@ -195,15 +195,18 @@
           $position = strpos($message, "3")
         ?>
         <p>$message = "0123456789"<br>$position = strpos($message, "3") <br>echo $position = <span><?= $position ?></span></p>
-        <p>Leerzeichen werden auch gezählt / Indexwertung heißt ab 0 / Immer der Anfangsbuchstabe - Zahl wird gezeigt</p>
+        <p> ----------------------------------------------------</p>
         <?php 
           $message1 = "Sei Gerecht auf alles";
           $position1 = strpos($message1, "Gerecht")
         ?>
         <p>$message1 = "Sei Gerecht auf alles"<br>$position1 = strpos($message1, "Gerecht") <br>echo $position1 = <span><?= $position1 ?></span></p>
         <p>echo $message1[4] = <span><?= $message1[4] ?></span></p>
+        <p> ----------------------------------------------------</p>
+        <p> ++ Leerzeichen werden auch gezählt / Indexwertung heißt ab 0 / Immer der Anfangsbuchstabe - Zahl wird gezeigt /Achtet auf Groß und kleinbuchstaben ++</p>
       </div>
-      <!-- strlen -->
+
+      <!-- strpos offset -->
       <div class="box">
         <h4>strpos offset</h4>
         <?php 
@@ -213,6 +216,80 @@
         <p>$message2 = "Sei Gerecht auf alles"<br>$position2 = strpos($message2, "Gerecht",3) <br> die 3 Bedeutet - Fängt bei index 3 zu suchen an <br>echo $position2 = <span><?= $position2 ?></span></p>
         
       </div>
+      <!-- stripos offset -->
+      <div class="box">
+        <h4>stripos offset</h4>
+        <?php 
+          $message3 = "Sei Gerecht auf alles";
+          $position3 = stripos($message3, "gerecht",3)
+        ?>
+        <p>$message3 = "Sei Gerecht auf alles"<br>$position3 = strpos($message3, "gerecht",3) <br> die 3 Bedeutet - Fängt bei index 3 zu suchen an <br>echo $position3 = <span><?= $position3 ?></span></p>
+        <p>++ stripos - Ignoriert Groß und kleinbuchstaben ++</p>
+        
+      </div>
+    </div>
+  </section>
+
+  <!-- 7. Wort ersetzen mit str_replace -->
+  <section id="7" class="section7">
+    <div class="text-box">
+      <h2>7. Wort / Zahl ersetzen str_replace - str_ireplace</h2>
+      <h6><a href="#0">Inhaltsverzeichnis</a></h6>
+    </div>
+    <div class="inhalt-container">
+
+      <!-- str_replace -->
+      <div class="box">
+        <h4>str_replace</h4>
+        <?php 
+          $nachricht = "Logik ist ein Wert von 12 Werten";
+          $string = str_replace("Logik", "Gleichheit", $nachricht);
+          
+        ?>
+        <p>$nachricht = "Logik ist ein Wert von 12 Werten" <br> $string = str_replace("Logik", "Gleichheit", $nachricht) <br>echo $string = <br> <span><?= $string ?></span></p>
+        <p>++ Achtet auf Groß oder Kleinschreibung ++</p>
+        
+       </div>
+
+       <!-- str_replace -->
+      <div class="box">
+        <h4>str_replace und zeige wieviele Ersetzungen</h4>
+        <?php 
+          $nachricht1 = "Logik ist ein Wert von 12 Werten";
+          $string1 = str_replace("Logik", "Tapferkeit", $nachricht1, $count);
+          
+        ?>
+        <p>$nachricht1 = "Logik ist ein Wert von 12 Werten" <br> $string1 = str_replace("Logik", "Tapferkeit", $nachricht1, $count) <br>echo $string1 . "br" . "Wieviel Wörter wurden ersetzt: " . $count = <br> <span><?= $string1 . "<br>" . "Wieviel Wörter wurden ersetzt: " .$count  ?></span></p>
+        <p>++ Achtet auf Groß oder Kleinschreibung ++</p>
+        
+       </div>
+
+       <!-- str_replace -->
+      <div class="box">
+        <h4>str_replace ( mehrere Wörter / Zahlen )</h4>
+        <?php 
+          $nachricht2 = "Logik ist ein Wert von 12 Werten";
+          $string2 = str_replace(["Logik", "ein"], ["Selbsterkennung",1,], $nachricht2, $count1);
+          
+        ?>
+        <p>$nachricht2 = "Logik ist ein Wert von 12 Werten" <br> $string2 = str_replace(["Logik", "ein"], ["Selbsterkennung",1,], $nachricht2, $count1) <br> necho $string2 . "br" . "Wieviel Wörter wurden ersetzt: " . $count = <br> <span><?= $string2 . "<br>" . "Wieviel Wörter wurden ersetzt: " .$count1  ?></span></p>
+        <p>++ Achtet auf Groß oder Kleinschreibung ++</p>
+        
+       </div>
+
+       <!-- str_ireplace -->
+      <div class="box">
+        <h4>str_ireplace</h4>
+        <?php 
+          $nachricht3 = "Logik ist ein Wert von 12 Werten";
+          $string3 = str_ireplace("logik", "Tugend", $nachricht3);
+          
+        ?>
+        <p>$nachricht3 = "Logik ist ein Wert von 12 Werten" <br> $string3 = str_ireplace("Logik", "Tugend", $nachricht3) <br> ++ i = Ignoriert Groß oder Kleinschreibung ++ <br> echo $string3 = <span><?= $string3 ?></span></p>
+        
+        
+       </div>
+
     </div>
   </section>
 
