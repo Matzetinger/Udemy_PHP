0b36c4ff8e7b142bc4047207847ebc23ce40bf62
Matthias Mühlender <muehlenderm@googlemail.com>
2025-11-27 21:39:29 +0100

    Update

--- Abschnitt_31/Video-Section/index.php
+++ Abschnitt_31/Video-Section/index.php
@@ -23,10 +23,10 @@
         <li><a href="#5">05. Stringlänge strlen</a></li>
         <li><a href="#6">06. Stringposition strpos - stripos</a></li>
         <li><a href="#7">07. Wort / Zahl ersetzen str_replace - str_ireplace</a></li>
-        <li><a href="#8">08. </a></li>
-        <li><a href="#9">09. </a></li>
-        <li><a href="#10">10. </a></li>
-        <li><a href="#11">11. </a></li>
+        <li><a href="#8">08. Stringteile ausschneiden substr</a></li>
+        <li><a href="#9">09. Klein und Großschreibung strtolower und strtoupper</a></li>
+        <li><a href="#10">10. Stringleerzeichen / Zeichen entfernen mit trim</a></li>
+        <li><a href="#11">11. Strings trennen mit explode</a></li>
         <li><a href="#12">12. </a></li>
         <li><a href="#13">13. </a></li>
         <li><a href="#14">14. </a></li>
@@ -214,8 +214,8 @@
           $position2 = strpos($message2, "Gerecht",3)
         ?>
         <p>$message2 = "Sei Gerecht auf alles"<br>$position2 = strpos($message2, "Gerecht",3) <br> die 3 Bedeutet - Fängt bei index 3 zu suchen an <br>echo $position2 = <span><?= $position2 ?></span></p>
-        
       </div>
+
       <!-- stripos offset -->
       <div class="box">
         <h4>stripos offset</h4>
@@ -225,7 +225,6 @@
         ?>
         <p>$message3 = "Sei Gerecht auf alles"<br>$position3 = strpos($message3, "gerecht",3) <br> die 3 Bedeutet - Fängt bei index 3 zu suchen an <br>echo $position3 = <span><?= $position3 ?></span></p>
         <p>++ stripos - Ignoriert Groß und kleinbuchstaben ++</p>
-        
       </div>
     </div>
   </section>
@@ -248,8 +247,7 @@
         ?>
         <p>$nachricht = "Logik ist ein Wert von 12 Werten" <br> $string = str_replace("Logik", "Gleichheit", $nachricht) <br>echo $string = <br> <span><?= $string ?></span></p>
         <p>++ Achtet auf Groß oder Kleinschreibung ++</p>
-        
-       </div>
+      </div>
 
        <!-- str_replace -->
       <div class="box">
@@ -261,8 +259,7 @@
         ?>
         <p>$nachricht1 = "Logik ist ein Wert von 12 Werten" <br> $string1 = str_replace("Logik", "Tapferkeit", $nachricht1, $count) <br>echo $string1 . "br" . "Wieviel Wörter wurden ersetzt: " . $count = <br> <span><?= $string1 . "<br>" . "Wieviel Wörter wurden ersetzt: " .$count  ?></span></p>
         <p>++ Achtet auf Groß oder Kleinschreibung ++</p>
-        
-       </div>
+      </div>
 
        <!-- str_replace -->
       <div class="box">
@@ -274,8 +271,7 @@
         ?>
         <p>$nachricht2 = "Logik ist ein Wert von 12 Werten" <br> $string2 = str_replace(["Logik", "ein"], ["Selbsterkennung",1,], $nachricht2, $count1) <br> necho $string2 . "br" . "Wieviel Wörter wurden ersetzt: " . $count = <br> <span><?= $string2 . "<br>" . "Wieviel Wörter wurden ersetzt: " .$count1  ?></span></p>
         <p>++ Achtet auf Groß oder Kleinschreibung ++</p>
-        
-       </div>
+      </div>
 
        <!-- str_ireplace -->
       <div class="box">
@@ -286,10 +282,175 @@
           
         ?>
         <p>$nachricht3 = "Logik ist ein Wert von 12 Werten" <br> $string3 = str_ireplace("Logik", "Tugend", $nachricht3) <br> ++ i = Ignoriert Groß oder Kleinschreibung ++ <br> echo $string3 = <span><?= $string3 ?></span></p>
-        
-        
-       </div>
+      </div>
+    </div>
+  </section>
 
+  <!-- 8. Stringteile ausschneiden substr-->
+  <section id="8" class="section8">
+    <div class="text-box">
+      <h2>8. Stringteile ausschneiden substr</h2>
+      <h6><a href="#0">Inhaltsverzeichnis</a></h6>
+    </div>
+    <div class="inhalt-container">
+
+      <!-- substr -->
+      <div class="box">
+        <h4>substr</h4>
+        <?php 
+          $nachricht8 = "Glückseligkeit ist ein Wert von 12 Werten";
+          $part = substr($nachricht8, 0,15);
+        ?>
+        <p>$nachricht8 = "Glückseligkeit ist ein Wert von 12 Werten" <br> $part = substr($nachricht8, 0, 15) <br> -------------------------------------- <br> Beginnt bei 0 bis zeichen 15 (15 wegen dem ü) <br>echo $part = <span><?= $part ?></span></p>
+      </div>
+
+      <!-- substr -->
+      <div class="box">
+        <h4>substr von hinten ("-" Wert)</h4>
+        <?php 
+          $message8 = "Wahrheit ist ein Wert von 12 Werten";
+          $part1 = substr($message8, -18, 4);
+        ?>
+        <p>$message8 = "Wahrheit ist ein Wert von 12 Werten" <br> $part1 = substr($message8, -18, 4) <br> -------------------------------------- <br> von hinten -18 zeichen und 4 Zeichen nach vorne <br> echo $part1 = <span><?= $part1 ?></span></p>
+      </div>
+    </div>
+  </section>
+
+  <!-- 9. Groß und Kleinschreibung strtolower und strtoupper -->
+  <section id="9" class="section9">
+    <div class="text-box">
+      <h2>9. Klein und Großschreibung strtolower und strtoupper</h2>
+      <h6><a href="#0">Inhaltsverzeichnis</a></h6>
+    </div>
+    <div class="inhalt-container">
+
+      <!-- strtolower -->
+      <div class="box">
+        <h4>strtolower</h4>
+        <?php 
+          $nachricht9 = "Der Unrechtsstaat ist klein";
+          $klein = strtolower($nachricht9);
+          $nachricht10 = "Der Unrechsstaat ist kleiner wie eine Bakterie"
+        ?>
+        <p>$nachricht9 = "Der Unrechtsstaat ist klein" <br> $klein = strtolower($nachricht9) <br> -------------------------------------- <br>echo $klein = <span><?= $klein ?></span></p>
+        <p> -------------------------------------- </p>
+        <p>$nachricht10 = "Der Unrechsstaat ist kleiner wie eine Bakterie" <br> echo strtolower($nachricht10) = <span><?= strtolower($nachricht10) ?></span> </p>
+      </div>
+
+      <!-- strtoupper -->
+      <div class="box">
+        <h4>strtoupper</h4>
+        <?php 
+          $nachricht11 = "Der Vollkommene Mann zeigt Größe";
+          $groß = strtoupper($nachricht10);
+          $nachricht11 = "Der Vollkommene Mann ist Iosif"
+        ?>
+        <p>$nachricht11 = "Der Vollkommene Mann zeigt Größe" <br> $groß = strtoupper($nachricht11) <br> -------------------------------------- <br>echo $groß = <span><?= $groß ?></span></p>
+        <p> -------------------------------------- </p>
+        <p>$nachricht11 = "Der Vollkommene Mann ist Iosif" <br> echo strtoupper($nachricht11) = <span><?= strtoupper($nachricht11) ?></span> </p>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- 10. Stringzeichen entfernen mit trim -->
+  <section id="10" class="section10">
+    <div class="text-box">
+      <h2>10. Stringleerzeichen / Zeichen entfernen mit trim - ltrim - rtrim</h2>
+      <h6><a href="#0">Inhaltsverzeichnis</a></h6>
+    </div>
+    <div class="inhalt-container">
+
+      <!-- trim -->
+      <div class="box">
+        <h4>trim</h4>
+        <?php 
+          $mitLeerzeichen = " Ethik ist ein Wert von 12 Werten ";
+          $ohneLeerzeichen = trim($mitLeerzeichen);
+        ?>
+        <p>$mitLeerzeichen = " Ethik ist ein Wert von 12 Werten " <br> $ohneLeerzeichen = trim($mitLeerzeichen); <br> -------------------------------------- <br> var_dump($ohneLeerzeichen) = <span><?php var_dump($ohneLeerzeichen); ?></span></p>
+        <p>var_dump($mitLeerzeichen) = <span><?php var_dump($mitLeerzeichen); ?></span></p>
+        <p>Für Datenbanken um leerzeichen zu vermeiden</p>
+      </div>
+
+      <!-- trim Zeichen-->
+      <div class="box">
+        <h4>trim Zeichen</h4>
+        <?php 
+          $mitZeichen = "!?Ethik ist ein Wert von 12 Werten!! %";
+          $ohneZeichen = trim($mitZeichen,"?!% ");
+        ?>
+        <p>$mitZeichen = "!?Ethik ist ein Wert von 12 Werten!! %" <br> $ohneZeichen = trim($mitZeichen,"?!% "); <br> -------------------------------------- <br> var_dump($ohneZeichen) = <span><?php var_dump($ohneZeichen); ?></span></p>
+        <p>var_dump($mitZeichen) = <span><?php var_dump($mitZeichen); ?></span></p>
+        
+      </div>
+
+      <!-- ltrim Vorne ohne Zeichen-->
+      <div class="box">
+        <h4>ltrim left ohne Zeichen</h4>
+        <?php 
+          $mitZeichen1 = "!?Ethik ist ein Wert von 12 Werten!! %";
+          $vorneOhneZeichen = ltrim($mitZeichen1,"?!% ");
+        ?>
+        <p>$mitZeichen1 = "!?Ethik ist ein Wert von 12 Werten!! %" <br> $vorneOhneZeichen = ltrim($mitZeichen1,"?!% "); <br> -------------------------------------- <br> var_dump($mitZeichen1) = <span><?php var_dump($mitZeichen1); ?></span> </p>
+        <p>var_dump($vorneOhneZeichen) = <span><?php var_dump($vorneOhneZeichen); ?></span></p>
+        
+      </div>
+
+      <!-- rtrim Hinten ohne Zeichen-->
+      <div class="box">
+        <h4>rtrim right ohne Zeichen</h4>
+        <?php 
+          $mitZeichen2 = "!?Ethik ist ein Wert von 12 Werten!! %";
+          $hintenOhneZeichen = rtrim($mitZeichen2,"?!% ");
+        ?>
+        <p>$mitZeichen2 = "!?Ethik ist ein Wert von 12 Werten!! %" <br> $hintenOhneZeichen = rtrim($mitZeichen2,"?!% "); <br> -------------------------------------- <br> var_dump($mitZeichen2) = <span><?php var_dump($mitZeichen2); ?></span> </p>
+        <p>var_dump($hintenOhneZeichen) = <span><?php var_dump($hintenOhneZeichen); ?></span></p>
+        
+      </div>
+    </div>
+  </section>
+
+  <!-- 11. String miteinander Verbinden -->
+  <section id="11" class="section11">
+    <div class="text-box">
+      <h2>11. Strings trennen mit explode</h2>
+      <h6><a href="#0">Inhaltsverzeichnis</a></h6>
+    </div>
+    <div class="inhalt-container">
+
+      <!-- explode -->
+      <div class="box">
+        <h4>explode</h4>
+        <?php 
+          $werte = "Harmonie,Gleichheit,Gerechtigkeit,Tapferkeit,Glückseligkeit,Logik,Ethik";
+          $trennen = explode(",",$werte);
+
+        ?>
+        <p>$werte = "Harmonie,Gleichheit,Gerechtigkeit,Tapferkeit,Glückseligkeit,Logik,Ethik"<br>$trennen = explode(",",$werte) <br> var_export($trennen) = <span><?php var_export($trennen);  ?></span></p>
+      </div>
+
+      <!-- explode Index -->
+      <div class="box">
+        <h4>explode index abrufen []</h4>
+        <?php 
+          $werte1 = "Harmonie,Gleichheit,Gerechtigkeit,Tapferkeit,Glückseligkeit,Logik,Ethik";
+          $trennen1 = explode(",",$werte1);
+
+        ?>
+        <p>$werte1 = "Harmonie,Gleichheit,Gerechtigkeit,Tapferkeit,Glückseligkeit,Logik,Ethik"<br>$trennen1 = explode(",",$werte1) <br> var_export($trennen1[2]) = <span><?php var_export($trennen1[2]);  ?></span></p>
+      </div>
+
+      <!-- explode Anzahl der Trennungen -->
+      <div class="box">
+        <h4>explode Anzahl der Trennungen</h4>
+        <?php 
+          $werte2 = "Harmonie,Gleichheit,Gerechtigkeit,Tapferkeit,Glückseligkeit,Logik,Ethik";
+          $trennen2 = explode(",",$werte2, 3);
+
+        ?>
+        <p>$werte2 = "Harmonie,Gleichheit,Gerechtigkeit,Tapferkeit,Glückseligkeit,Logik,Ethik"<br>$trennen2 = explode(",",$werte2, 3) <br> var_export($trennen2) = <span><?php var_export($trennen2);  ?></span></p>
+      </div>
     </div>
   </section>
 
