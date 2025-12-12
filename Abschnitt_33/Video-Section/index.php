cfedb953acc2adcb11601329831bb28469751148
Matthias Mühlender <muehlenderm@googlemail.com>
2025-12-12 19:56:07 +0100

    Update

--- Abschnitt_33/Video-Section/index.php
+++ Abschnitt_33/Video-Section/index.php
@@ -16,8 +16,8 @@
     <div class="box">
     <h2>Inhaltsverzeichnis</h2>
       <ul>
-        <li><a href="#1">01. for-Schleife </a></li>
-        <li><a href="#2">02. </a></li>
+        <li><a href="#1">01. for-Schleife</a></li>
+        <li><a href="#2">02. while-Schleife</a></li>
         <li><a href="#3">03. </a></li>
       </ul>
     </div>
@@ -48,7 +48,7 @@
         <h4>for im Beispiel mit 3 Schleifen</h4>
         
           <pre>
-            for($i;$i < 3;){ 
+            <span class="rot">for</span>($i;$i < 3;){ 
               echo "Start $schleife_start. Schleife < br >";
 
                $s = $i++;
@@ -83,7 +83,7 @@
         <h4>for im Beispiel mit Array</h4>
         
           <pre>
-           for($w=0; $w < count($wert); $w++){ 
+           <span class="rot">for</span>($w=0; $w < count($wert); $w++){ 
 
             echo $wert[$w] . "< br >";
            }
@@ -99,6 +99,138 @@
           </p>       
       </div>
 
+    </div>
+  </section>
+
+  <!-- 1. While-Schleife -->
+  <section id="2" class="section">
+    <div class="text-box">
+      <h2>1. while-Schleife</h2>
+        <p><a href="#0">Inhaltsverzeichnis</a></p>
+      
+      <!-- Variablen -->
+      <h4>Variablen</h4>
+        <?php 
+         $a = 0;
+         $t = true;
+         $tr = true;
+        ?>
+        <p>$a = 0; <br> $t = true; <br> $tr = true; </p>
+    </div>
+
+    <div class="inhalt-container">
+
+      <!-- While Schleife -->
+      <div class="box">
+        <h4>while </h4>
+        
+          <pre>
+            <span class="rot">while</span> ($a < 3) {
+                    echo '$a = ' . $a . " / ";
+
+                  $a++;
+
+                    echo '$a++ = ' . $a . "< br >";
+                    echo "code ende < br > < br >";
+                 }
+            
+          </pre>
+          <p style="text-align:center">
+            <span>
+              <?php
+                 while ($a < 3) {
+                    echo '$a = ' . $a . " / ";
+
+                  $a++;
+
+                    echo '$a++ = ' . $a . "<br>";
+                    echo "code ende <br> <br>";
+                 }
+              ?>
+            </span>
+           Bei while (Nur bedingung) dann { code }
+          </p>       
+      </div>
+
+      <!-- While Schleife -->
+      <div class="box">
+        <h4>while </h4>
+        
+          <pre>
+            <span class="rot">while</span> ($t) {
+                    var_dump ($t);
+                    echo "< br > Hier ist \$t = ($t) noch true < br >";
+                    $t = false;
+                    echo "< br > hier ist \$t = ($t) false < br >";
+                    var_dump ($t);
+                 }
+            
+          </pre>
+          <p style="text-align:center">
+            <span>
+              <?php
+                 while ($t) {
+                    var_dump ($t);
+                    echo "<br> Hier ist \$t = ($t) noch true <br>";
+                    $t = false;
+                    echo "<br> hier ist \$t = ($t) false <br>";
+                    var_dump ($t);
+                 }
+              ?>
+            </span>
+            <br> <br> 1. Durchgang, erst wird geprüft $t = true <br> dann $t wird auf false gesetzt <br> nächste prüfung = $t false, Ende
+          </p>       
+      </div>
+
+      <!-- While Schleife verschachtelt -->
+      <div class="box">
+        <h4>While Schleife verschachtelt </h4>
+        
+          <pre>
+            while ($tr) {
+                    var_dump ($tr);
+                    echo "< br > Hier ist \$tr = ($tr) noch true < br >";
+
+                      for($b=0; $b < 3;$b++){
+                        echo "hier beginnt for-Schleife \$b= $b < 3 < br >";
+
+                        if ($b === 2) {
+                          echo "for schleife ende";
+                        }
+                        
+                      } 
+                      
+                    $tr = false;
+                    echo "< br > hier ist \$tr = ($tr) false < br >";
+                    var_dump ($tr);
+                 }
+            
+          </pre>
+          <p style="text-align:center">
+            <span>
+              <?php
+                 while ($tr) {
+                    var_dump ($tr);
+                    echo "<br> Hier ist \$tr = ($tr) noch true <br>";
+
+                      for($b=0; $b<3;$b++){
+                        echo "hier beginnt for-Schleife \$b= $b < 3 <br>";
+
+                        if ($b === 2) {
+                          echo "for schleife ende";
+                        }
+
+                      } 
+                      
+                    $tr = false;
+                    echo "<br> hier ist \$tr = ($tr) false <br>";
+                    var_dump ($tr);
+                 }
+              ?>
+            </span>
+            <br> <br> Start <br> erst wird geprüft ist $tr = true <br> oke $tr = true <br> dann for Schleife bis $tr < 3 <br> $tr === 2 dann "for schleife ende" <br> $t wird auf false gesetzt <br> nächste prüfung while ist $t noch true? Nein $t = false <br> Ende
+          </p>      
+      </div>
     </div>
   </section>
 
