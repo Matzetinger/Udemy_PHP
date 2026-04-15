fd969a674dac1c32975459c0fe2b66835ed12b8b
Matthias Mühlender <muehlenderm@googlemail.com>
2026-04-15 20:56:44 +0200

    Update

---
+++ Abschnitt_42/Countdownzaehler/forms/form.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+  <label for="date">Dein Datum</label>
+  <input
+    type="datetime-local"
+     name="date"
+     id="date"
+  >
+  <button type="submit">Calculate</button>
+</form>
--- Abschnitt_42/Countdownzaehler/index.php
+++ Abschnitt_42/Countdownzaehler/index.php
@@ -1,3 +1,32 @@
+<?php
+
+function validateDateTime($date, $format = "Y-m-d\TH:i"){
+  $d = Datetime::createFromFormat($format, $date);
+  return $d && $d->format($format) === $date;
+}
+
+function getDiff($userDate){
+  $current_Date = new DateTime();
+  $userDateObj = new DateTime($userDate);
+  $diff = $userDateObj->diff($current_Date);
+
+  return $diff;
+}
+
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+  $userDate = $_POST["date"];
+
+  if(validateDateTime($userDate)){
+    $res = getDiff($userDate);
+    var_dump($res);
+  }else{
+    echo "Error";
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -5,14 +34,18 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="author" content="Matthias">
-  <meta name="keywords" content="Countdown, ">
-  <meta name="description" content="Übung wie man einen Countdownzähler mit PHP realisiert ">
+  <meta name="keywords" content="Countdown,Datum, Zähler ">
+  <meta name="description" content="Übung wie man einen Datum Countdown Zähler mit PHP realisiert ">
   <meta name="robots" content="index, follow">
-  <title>Countdownzähler</title>
+  <title>Datum Countdown Zähler</title>
   <!-- Ressourcen -->
   <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
+
+<?php
+require_once("./forms/form.php");
+?>
 
 </body>
 </html>
