e76f94b8d21f99a1369b0d694955110199b6ed9e
Matthias Mühlender <muehlenderm@googlemail.com>
2026-04-17 22:10:38 +0200

    Update

--- Abschnitt_42/Countdownzaehler/index.php
+++ Abschnitt_42/Countdownzaehler/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$years = $month = $days = $hours = $minutes = $seconds = null;
+
 function validateDateTime($date, $format = "Y-m-d\TH:i"){
   $d = Datetime::createFromFormat($format, $date);
   return $d && $d->format($format) === $date;
@@ -13,15 +15,33 @@ function getDiff($userDate){
   return $diff;
 }
 
+function isFutureDate($date){
+  $now = new DateTime();
+  $inputDate = new DateTime($date);
+  return $inputDate > $now;
+}
+
 if($_SERVER["REQUEST_METHOD"] === "POST"){
   $userDate = $_POST["date"];
 
   if(validateDateTime($userDate)){
-    $res = getDiff($userDate);
-    var_dump($res);
+    if(isFutureDate($userDate)){
+      $diff = getDiff($userDate);
+
+      $years = $diff->y;
+      $month = $diff->m;
+      $days = $diff->d;
+      $hours = $diff->h;
+      $minutes = $diff->i;
+      $seconds = $diff->s;
+    }else{
+      echo "Date is not in the future";
+    }
+
   }else{
     echo "Error";
   }
+
 }
 
 ?>
@@ -43,9 +63,44 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
 </head>
 <body>
 
-<?php
-require_once("./forms/form.php");
-?>
+<div class="card-container">
+  <div class="title-container">
+    <h1>Wie viele Tage</h1>
+  </div>
+  <div class="time-container">
+    <div class="time-section">
+      <p class="time-title">Jahre</p>
+      <p class="time-count"><?php echo sprintf("%02d", $years)  ?> </p>
+    </div>
+    <div class="time-section">
+      <p class="time-title">Monate</p>
+      <p class="time-count"><?php echo sprintf("%02d", $month) ?></p>
+    </div>
+    <div class="time-section">
+      <p class="time-title">Tage</p>
+      <p class="time-count"><?php echo sprintf("%02d", $days) ?> </p>
+    </div>
+    <div class="time-section">
+      <p class="time-title">Stunden</p>
+      <p class="time-count"><?php echo sprintf("%02d", $hours) ?> </p>
+    </div>
+    <div class="time-section">
+      <p class="time-title">Minuten</p>
+      <p class="time-count"><?php echo sprintf("%02d", $minutes) ?> </p>
+    </div>
+    <div class="time-section">
+      <p class="time-title">Sekunden</p>
+      <p class="time-count"><?php echo sprintf("%02d", $seconds) ?> </p>
+    </div>
+  </div>
+  <div class="form-container">
+    <?php require_once("./src/forms/form.php");?>
+    <small class="err">Fehler</small>
+  </div>
+</div>
+
+
+
 
 </body>
 </html>
